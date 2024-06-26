c455ea709c77d333132a94e74eef0cc9e0c1e177
Rostislav Kouznetsov <rostislav.kouznetsov@fmi.fi>
2024-06-26 14:41:50 +0300

    Simple Fire-source script based on 7d data stream

--- NRT-fires-INDIA.sh
+++ NRT-fires-INDIA.sh
@@ -6,10 +6,19 @@ metadata=fire_metadata_ecodata_Akagi_continents_withAVB_v2.ini
 mask=masks-5w/MOD61-2023-mask-5w.nc
 
 
-infiles="NRT-ASIA/MODIS_C6_1_SouthEast_Asia_MCD14DL_NRT_202417?.txt"
+
+region=South_Asia
 
 
-python3 FIRMS_cluster.py -v --metadata $metadata --mask=masks-5w/MOD61-2023-mask-5w.nc --extend=5  NRT-ASIA/MODIS_C6_1_South_Asia_MCD14DL_NRT_202417?.txt out.india-ext5.csv
+
+infiles="NRT-ASIA/MODIS_C6_1_South_Asia_7d.csv"
+
+julday=date
+
+for d in `seq -7 4`; do
+  outd=`date -u  -d "$d days" +%Y-%m-%d`
+  echo python3 FIRMS_cluster.py -v --metadata $metadata --mask=$mask --extend=7 $infiles --datepref=$outd  out.india-$outd.csv
+done | xargs -P 20 -I{} sh -c '{}'
 
 #usage: FIRMS_cluster.py [-h] [--metadata METADATA] [--datepref DATEPREF]
 #                        [--mask MASK] [--extend EXTEND] [--rmse-fit]
